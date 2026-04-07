759dc1b221b1ad242d7584eb4ae2ab39f36510c9
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-07 16:48:45 +1000

    fix: fix sshd

--- modules/sshd.nix
+++ modules/sshd.nix
@@ -11,6 +11,7 @@ in
     $DRY_RUN_CMD chmod $VERBOSE_ARG 700 "${config.user.home}/.ssh"
     $DRY_RUN_CMD chmod $VERBOSE_ARG 600 "${config.user.home}/.ssh/authorized_keys"
 
+    $DRY_RUN_CMD mkdir $VERBOSE_ARG --parents "${config.user.home}/.local/var/log"
     $DRY_RUN_CMD mkdir $VERBOSE_ARG --parents "${sshdDirectory}"
 
     if [[ ! -f "${sshdDirectory}/ssh_host_ed25519_key" ]]; then
@@ -20,6 +21,17 @@ in
 
     $VERBOSE_ECHO "Writing sshd_config..."
     $DRY_RUN_CMD echo -e "HostKey ${sshdDirectory}/ssh_host_ed25519_key\nPort ${toString port}\nPubkeyAuthentication yes\nAuthenticationMethods publickey\nAuthorizedKeysFile ${config.user.home}/.ssh/authorized_keys\nStrictModes no\nPasswordAuthentication no\nKbdInteractiveAuthentication no\nChallengeResponseAuthentication no\nUsePAM no\n" > "${sshdDirectory}/sshd_config"
+
+    # Kill any existing sshd processes
+    if ${pkgs.procps}/bin/pgrep -x sshd > /dev/null; then
+      $VERBOSE_ECHO "Restarting sshd..."
+      $DRY_RUN_CMD ${pkgs.killall}/bin/killall -q sshd || true
+    else
+      $VERBOSE_ECHO "Starting sshd..."
+    fi
+
+    # Start sshd in the background
+    $DRY_RUN_CMD ${pkgs.runtimeShell} -lc 'nohup ${pkgs.openssh}/bin/sshd -f "${sshdDirectory}/sshd_config" -D >> "${config.user.home}/.local/var/log/sshd.log" 2>&1 &'
   '';
 
   environment.packages = [
