917c16222640bee01d7c8d1fe235db20713b5f77
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-07 16:45:28 +1000

    fix: allow local building

--- modules/nix.nix
+++ modules/nix.nix
@@ -5,7 +5,7 @@
     experimental-features = nix-command flakes
 
     # Always build on remote
-    max-jobs = 0
+    # max-jobs = 0
     builders = builder aarch64-linux - 4 1 nixos-test,big-parallel,kvm
   '';
 }
