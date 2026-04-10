c748fba505c14fd9051a2871fc31869c1f18c342
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-11 08:01:59 +1000

    refactor: disable default ssh config

--- home.nix
+++ home.nix
@@ -10,7 +10,20 @@
 
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
     matchBlocks = {
+      "*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
       "builder" = {
         # utm-vm
         hostname = "100.95.108.111";
