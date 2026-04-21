e5bf1f847776b0ad6ea3d84111898e01e6d0201c
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-21 14:07:13 +1000

    feat: add agentix

---
+++ secrets/secrets.nix
@@ -0,0 +1,9 @@
+let
+  poco-x3-pro = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKc5qFBtTUoJyj3LGHNOEdoOlpl1jfIE/3pElxH32m54";
+in
+{
+  # "cloudflared.age" = {
+  #   publicKeys = [ poco-x3-pro ];
+  #   armor = true;
+  # };
+}
