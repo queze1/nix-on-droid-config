69600c0b8eb29f55600d8ac40b2daa1e890e88d9
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-21 14:13:46 +1000

    Revert "feat: add agentix"
    This reverts commit e5bf1f847776b0ad6ea3d84111898e01e6d0201c.

--- secrets/secrets.nix
+++
@@ -1,9 +0,0 @@
-let
-  poco-x3-pro = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKc5qFBtTUoJyj3LGHNOEdoOlpl1jfIE/3pElxH32m54";
-in
-{
-  # "cloudflared.age" = {
-  #   publicKeys = [ poco-x3-pro ];
-  #   armor = true;
-  # };
-}
