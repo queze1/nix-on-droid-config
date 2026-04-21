09a62ae623630c1287bf84afbb22a3f6a851d3db
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-21 17:42:13 +1000

    fix: fix age file name

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,8 +2,8 @@ let
   poco-x3-pro = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKc5qFBtTUoJyj3LGHNOEdoOlpl1jfIE/3pElxH32m54";
 in
 {
-  # "cloudflared.age" = {
-  #   publicKeys = [ poco-x3-pro ];
-  #   armor = true;
-  # };
+  "cloudflare-tunnel-token.age" = {
+    publicKeys = [ poco-x3-pro ];
+    armor = true;
+  };
 }
