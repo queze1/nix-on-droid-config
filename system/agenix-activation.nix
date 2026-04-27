432c9eeb1869c0d80eef8768c6cca20c4e035330
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-27 15:26:11 +1000

    fix: temporarily disable agenix activation

--- system/agenix-activation.nix
+++ system/agenix-activation.nix
@@ -1,17 +1,16 @@
 { config, lib, ... }:
 {
-  options = {
-    systemd.user.services = lib.mkOption {
-      type = lib.types.attrsOf lib.types.unspecified;
-    };
-  };
+  # options = {
+  #   systemd.user.services = lib.mkOption {
+  #     type = lib.types.attrsOf lib.types.unspecified;
+  #   };
+  # };
 
-  config = {
-    # Use build.activation instead of systemd
-    build.activation.agenix = ''
-      echo "Decrypting secrets with agenix..."
-      echo ${config.systemd.user.services.agenix.Service.ExecStart}
-      $DRY_RUN_CMD ${config.systemd.user.services.agenix.Service.ExecStart}
-    '';
-  };
+  # config = {
+  #   # Use build.activation instead of systemd
+  #   build.activation.agenix = ''
+  #     echo "Decrypting secrets with agenix..."
+  #     $DRY_RUN_CMD ${config.systemd.user.services.agenix.Service.ExecStart}
+  #   '';
+  # };
 }
