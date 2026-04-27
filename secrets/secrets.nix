328e71fb72f66979c1ba016e0cac780b0921091a
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-27 12:48:35 +1000

    fix: use --token-file instead of --token, regenerate token

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,13 @@
 let
   poco-x3-pro = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKc5qFBtTUoJyj3LGHNOEdoOlpl1jfIE/3pElxH32m54";
+  utm-vm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINC3vA0PnFXyFRgitP7U8PL+SlTvqvE6eY73rpW5Rj4y ";
 in
 {
   "cloudflare-tunnel-token.age" = {
-    publicKeys = [ poco-x3-pro ];
+    publicKeys = [
+      poco-x3-pro
+      utm-vm
+    ];
     armor = true;
   };
 }
