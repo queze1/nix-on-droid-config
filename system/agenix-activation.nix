d3bcbcb789edda4c1a7ec54e968975ef34e073f2
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-27 14:06:15 +1000

    fix: set up xdg runtime directory

--- system/agenix-activation.nix
+++ system/agenix-activation.nix
@@ -17,5 +17,16 @@
       $VERBOSE_ECHO "Decrypting secrets with agenix..."
       $DRY_RUN_CMD ${config.systemd.user.services.agenix.Service.ExecStart}
     '';
+
+    # Set up temporary runtime directory
+    environment.sessionVariables = {
+      XDG_RUNTIME_DIR = "${config.user.home}/.run";
+    };
+
+    build.activation.setupRuntimeDir = ''
+      $DRY_RUN_CMD rm -rf ${config.user.home}/.run
+      $DRY_RUN_CMD mkdir -p ${config.user.home}/.run
+      $DRY_RUN_CMD chmod 700 ${config.user.home}/.run
+    '';
   };
 }
