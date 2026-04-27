12a942d1dd4c3332aea2575df1db12a04de1ddb1
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-27 15:59:09 +1000

    fix: turn list into a string

--- home/default.nix
+++ home/default.nix
@@ -46,7 +46,7 @@
   # Capture agenix systemd and run it as an activation script
   home.activation.agenix = ''
     echo "Decrypting secrets with agenix..."
-    $DRY_RUN_CMD ${config.systemd.user.services.agenix.Service.ExecStart}
+    $DRY_RUN_CMD ${config.systemd.user.services.agenix.Service.ExecStart.head}
   '';
 
   # Read the changelog before changing this value
