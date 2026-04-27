ae8b8043767728f683c1bbdbd4846190df9e2556
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-27 15:05:02 +1000

    fix: remove fake xdg runtime dir

--- system/agenix-activation.nix
+++ system/agenix-activation.nix
@@ -17,16 +17,5 @@
       $VERBOSE_ECHO "Decrypting secrets with agenix..."
       $DRY_RUN_CMD ${config.systemd.user.services.agenix.Service.ExecStart}
     '';
-
-    # Set up fake $XDG_RUNTIME_DIR since nix-on-droid doesn't have it
-    environment.sessionVariables = {
-      XDG_RUNTIME_DIR = "$HOME/.run";
-    };
-
-    build.activation.setupRuntimeDir = ''
-      $DRY_RUN_CMD rm -rf ${config.user.home}/.run
-      $DRY_RUN_CMD mkdir -p ${config.user.home}/.run
-      $DRY_RUN_CMD chmod 700 ${config.user.home}/.run
-    '';
   };
 }
