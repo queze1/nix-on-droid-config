d76aef530afb74b8d5417f4cfc60466a9644417b
queze1 <52340127+queze1@users.noreply.github.com>
2026-05-03 17:30:54 +1000

    fix: temporarily comment out runit-manager

--- home/runit.nix
+++ home/runit.nix
@@ -18,41 +18,41 @@ let
   cfg = config.services.runit;
   enabledServices = lib.filterAttrs (_: s: s.enable) cfg.services;
 
-  runit-manager = pkgs.writeShellScriptBin "runit-manager" ''
-    set -eu
-    service_dir=${escapeShellArg cfg.serviceDir}
-    log_file=${escapeShellArg "${cfg.logDir}/runsvdir.log"}
+  # runit-manager = pkgs.writeShellScriptBin "runit-manager" ''
+  #   set -eu
+  #   service_dir=${escapeShellArg cfg.serviceDir}
+  #   log_file=${escapeShellArg "${cfg.logDir}/runsvdir.log"}
 
-    case "''${1:-}" in
-      start)
-        if ! pgrep -x runsvdir >/dev/null; then
-          setsid runsvdir "$service_dir" >"$log_file" 2>&1 &
-          echo "runsvdir started"
-        else
-          echo "runsvdir already running"
-        fi
-        ;;
-      stop)
-        if pgrep -x runsvdir >/dev/null; then
-          pkill -HUP -x runsvdir
-          echo "runsvdir sent HUP"
-        else
-          echo "runsvdir not running"
-        fi
-        ;;
-      status)
-        if pgrep -x runsvdir >/dev/null; then
-          echo "runsvdir running"
-        else
-          echo "runsvdir not running"
-        fi
-        ;;
-      *)
-        echo "usage: $0 {start|stop|status}"
-        exit 1
-        ;;
-    esac
-  '';
+  #   case "''${1:-}" in
+  #     start)
+  #       if ! pgrep -x runsvdir >/dev/null; then
+  #         setsid runsvdir "$service_dir" > "$log_file" 2>&1 &
+  #         echo "runsvdir started"
+  #       else
+  #         echo "runsvdir already running"
+  #       fi
+  #       ;;
+  #     stop)
+  #       if pgrep -x runsvdir >/dev/null; then
+  #         pkill -HUP -x runsvdir
+  #         echo "runsvdir sent HUP"
+  #       else
+  #         echo "runsvdir not running"
+  #       fi
+  #       ;;
+  #     status)
+  #       if pgrep -x runsvdir >/dev/null; then
+  #         echo "runsvdir running"
+  #       else
+  #         echo "runsvdir not running"
+  #       fi
+  #       ;;
+  #     *)
+  #       echo "usage: $0 {start|stop|status}"
+  #       exit 1
+  #       ;;
+  #   esac
+  # '';
 
   mkServiceFiles =
     name: svc:
@@ -154,7 +154,7 @@ in
       "cd-log-dir" = "cd ${config.services.runit.logDir}";
     };
 
-    home.packages = [ runit-manager ];
+    # home.packages = [ runit-manager ];
 
     home.activation.runitDirectories = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       run --quiet mkdir --parents ${escapeShellArg cfg.serviceDir}
