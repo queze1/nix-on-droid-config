086e4efdd785f1a3e8e38a9aa07739a77248b994
queze1 <52340127+queze1@users.noreply.github.com>
2026-05-03 20:47:26 +1000

    fix: fix caddy cloudflare plugin tag

--- home/programs.nix
+++ home/programs.nix
@@ -4,7 +4,7 @@
     # Services
     (caddy.withPlugins {
       plugins = [
-        "github.com/caddy-dns/cloudflare@master"
+        "github.com/caddy-dns/cloudflare@v0.2.4"
       ];
       hash = "sha256-4WF7tIx8d6O/Bd0q9GhMch8lS3nlR5N3Zg4ApA3hrKw=";
     })
