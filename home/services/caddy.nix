3b772095253a011c242a89fb59e9f6658b9768b5
queze1 <52340127+queze1@users.noreply.github.com>
2026-06-18 10:53:18 +1000

    chore: update caddy cloudflare plugin hash

--- home/services/caddy.nix
+++ home/services/caddy.nix
@@ -16,7 +16,7 @@
     plugins = [
       "github.com/caddy-dns/cloudflare@v0.2.4"
     ];
-    hash = "sha256-uKtStb6m1/hA5IaAdIyLGzAQdyIySjISdxXIRxehhyI=";
+    hash = "sha256-bzMqxWTqrJ1skZmRTXyEMCKStXpljbqe5r0Ve2cnBfM=";
   };
 in {
   options.services.caddy = {
